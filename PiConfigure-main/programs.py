eb763626dbad0432dc1dcd722932a75f1b5bf2cd
PawelWrHkl <pawel.woroniecki.hkl@gmail.com>
2024-04-17 14:06:00 +0200

    PiConfigure

---
+++ PiConfigure-main/programs.py
@@ -0,0 +1,22 @@
+programs = {
+    'python2':
+        {
+            'KOD HD do szyn':'KOD_HD_szyny.py',
+            "KOD HD wycinanie lameli": 'KOD_HD_lamele.py',
+            'Pila do metalu':'pila_v_json.py',
+            "Wyswietlanie tkanin na rollo":'rollo_tkanina_v_json.py',
+            'Wyswietlanie tkanin na rollo-maszty':'tkanina_v_json.py',
+            'Wyswietlanie tkanin na plisach':'tkaniny_plisy.py',
+            'Komunikacja z maszyną do wycinania wertikale': 'Vertikali1.py',
+            'Wyswietlanie tkanin na wertikalach':'vertikali_tkaniny.py'
+        },
+    'python3':
+        {
+        'autokamera': 'autokamera.py',
+        'naped na  plisy': 'naped_plisy/plisy_program.py',
+        'Sygnalizator dzwiekowo-wizualny na pakowanie':'sygnalizator.py',
+        'Error Helper': 'ErrorHelper/autostart.sh',
+        "Wyswietlanie uchwytow":'Uchwyty/autostart.sh',
+        }
+
+}
